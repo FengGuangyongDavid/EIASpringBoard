65bb521a0b1b0d8d06aaf0b33288643094b6455e
liupu <lp1013139032@gmail.com>
2023-09-22 14:01:11 +0800

    update age api

--- src/main/java/com/spring/board/eia/controller/OutcomesController.java
+++ src/main/java/com/spring/board/eia/controller/OutcomesController.java
@@ -1,17 +1,21 @@
 package com.spring.board.eia.controller;
 
+import com.google.common.collect.Lists;
 import com.spring.board.eia.entity.Person;
 import com.spring.board.eia.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class OutcomesController {
     @Autowired
     private PersonRepository repository;
+
     @GetMapping("/getOutcomes")
     public String getOutcomes() {
 
@@ -27,6 +31,13 @@ public class OutcomesController {
         long connectedToMHProviderNo = personList.stream().filter(person -> !person.getMHProvider().isEmpty()).count();
         long documentationAssistanceNo = personList.stream().filter(person -> person.getDocumentationAssistance().equalsIgnoreCase("yes")).count();
         long getAchievementsNo = personList.stream().filter(person -> !person.getAchievements().isEmpty()).count();
+        Map<String,List> result = new HashMap<>();
+        result.put("category", Lists.newArrayList("#total","#getPermanentHouse",
+                "#incarcerated","#entrustedAssertiveCommunity",
+                "#livePeople","#connectedToMHProvider"));
+        result.put("data", Lists.newArrayList(totalPersonNo,getPermanentHouseNo,
+                incarceratedNo,entrustedAssertiveCommunityNo,
+                livePeopleNo,connectedToMHProviderNo));
 
 
         return null;
