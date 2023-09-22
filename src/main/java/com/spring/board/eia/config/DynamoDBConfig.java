a29860c7dc116f50c90bbd33421146c4eb9f2d06
Fengguangyong <249675362@qq.com>
2023-09-22 23:21:30 +0800

    addition of program outcome

--- src/main/java/com/spring/board/eia/config/DynamoDBConfig.java
+++ src/main/java/com/spring/board/eia/config/DynamoDBConfig.java
@@ -18,9 +18,6 @@ public class DynamoDBConfig {
     public static final String ACCESS_KEY = "";
     public static final String SECRET_KEY = "";
 
-//    public static final String ACCESS_KEY = "AKIAWQPN2BRUEQKQU5AP";
-//    public static final String SECRET_KEY = "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";
-
 
     @Bean
     public DynamoDBMapper mapper() {
