26cd11dbbff9702c4fccd2092838fab01d8b6334
Fengguangyong <249675362@qq.com>
2023-09-22 23:58:19 +0800

    app

--- src/main/java/com/spring/board/eia/config/DynamoDBConfig.java
+++ src/main/java/com/spring/board/eia/config/DynamoDBConfig.java
@@ -7,16 +7,15 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.context.annotation.Bean;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDBConfig {
 
     //public static final String SERVICE_ENDPOINT = "https://dynamodb-fips.us-east-1.amazonaws.com";
-    public static final String SERVICE_ENDPOINT = "";
-    public static final String REGION = "";
-    public static final String ACCESS_KEY = "";
-    public static final String SECRET_KEY = "";
+    public static final String SERVICE_ENDPOINT = "https://dynamodb.us-east-1.amazonaws.com";
+    public static final String REGION = "us-east-1";
 
 
     @Bean
@@ -28,6 +27,6 @@ public class DynamoDBConfig {
     private AmazonDynamoDB amazonDynamoDBConfig() {
         return AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY))).build();
+                .build();
     }
 }
